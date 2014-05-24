b2776427d91ac362111239c3679aba7ea7570f98
Pavi Kumbhar <pavikumbhar007@gmail.com>
2014-05-24 12:01:44 +0530

    Create CountPalindromWordInString1.c

---
+++ StringProgram/CountPalindromWordInString1.c
@@ -0,0 +1,106 @@
+/***********************************************************************
+ Progrom to Count Number of Palindrom Word  Present in  String .
+ ----------------------------------------------------------------------
+ Input: madam asked For nitin
+ Output: 2 ( eg. madam  and nitin)
+----------------------------------------------------------------------
+Author: Pavi Kumbhar B.C.S MCA[Sci]
+
+Catch Me at :www.facebook.com/pavi.Kumbhar
+
+************************************************************************/
+
+
+#include<stdio.h>
+#include<conio.h>
+
+
+char * xstrrev(char * ,int);
+int xstrcmp(char *, char *);
+int xstrlen(char *);
+
+void main()
+{
+ char str[100],str2[100];
+ char str3[100];
+ char *a;
+ int i,j,k=0,c,x,y,count=0;
+ clrscr();
+ printf("\n Enter the sting: ");
+ gets(str);
+ j=xstrlen(str);
+ for(i=0;i<=j;i++)
+   {
+	  if(str[i]==' '||str[i]=='\0')
+	  {
+
+	   str2[k]='\0';
+	   c=xstrlen(str2);
+	   a=xstrrev(str2,c);
+	   for(x=0;x<c;x++)
+		{
+		 str3[x]=*a;
+		 a-- ;
+		}
+
+		str3[x]='\0';
+
+	  if(xstrcmp(str2,str3)==0)
+	   count++;
+	   k=0;
+
+	  }
+	  else{
+
+		  str2[k++]=str[i];
+
+		 }
+
+
+   }
+   printf("No of Palindrom Word =%d",count);
+getch();
+
+}
+
+char * xstrrev(char *ss,int l)
+{
+ int i;
+ char *tt,temp,*t1;
+
+ tt=ss+l-1;
+ return tt;
+
+}
+
+// Function for Count Length of String 
+int xstrlen(char *s)
+{
+
+  int c=0;
+  while(*s)
+  {
+   s++;
+   c++;
+
+  }
+ return c;
+
+}
+
+//Function for Compare Two String
+int xstrcmp(char *s,char *t )
+  {
+	while(*s==*t)
+	{
+	 if(!(*s))
+	 return 0;
+	 s++;
+
+	 t++;
+
+	}
+return(*s-*t);
+
+  }
+
